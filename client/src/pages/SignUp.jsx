7e75466caf868792b1c85ca825587c5673137800
sthiti.suman <sahoosthitisuman@gmail.com>
2024-01-11 15:10:47 +0530

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  return <div></div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState();
+  const handleChnage = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      console.log(data);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  return (
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <Header />
+
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          maxWidth: "300px",
+        }}
+      >
+        <h1 className="text-3xl text-center font-semibold m-5 font-serif">
+          Sign Up
+        </h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChnage}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            id="email"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChnage}
+          />
+
+          <input
+            type="text"
+            placeholder="Password"
+            id="password"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChnage}
+          />
+          <button
+            disabled={loading}
+            className="bg-slate-700 text-white p-3 rounded-lg  hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading.." : "Signup"}
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p className="font-serif">Already have an account?</p>
+          <Link to="/sign-in">
+            <span className="text-blue-500">Sign in</span>
+          </Link>
+        </div>
+        <p className="text-red-700 mt-5">{error && "Something went wrong"}</p>
+      </div>
+    </div>
+  );
 }
