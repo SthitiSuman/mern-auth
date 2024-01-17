c6ccf5468787397bf0a47f3a09fb2dcf104f0e79
sthiti.suman <sahoosthitisuman@gmail.com>
2024-01-17 16:50:55 +0530

    add pre production changes

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,22 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use(express.json());
+
 app.use(cookieParser());
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -17,15 +28,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
 app.use("/api/user", userRoutes);
+
 app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
+
   return res.status(statusCode).json({
     success: false,
     message,
